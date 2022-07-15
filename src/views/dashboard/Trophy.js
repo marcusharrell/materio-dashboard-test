ea9539b531fca33bc697b341d9a672a366400e4e
Marcus <marcusharrell597@icloud.com>
2022-07-15 17:56:51 -0400

    fix trophy card

--- src/views/dashboard/Trophy.js
+++ src/views/dashboard/Trophy.js
@@ -1,9 +1,13 @@
 // ** MUI Imports
 import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import { styled, useTheme } from '@mui/material/styles'
+import IconButton from '@mui/material/IconButton'
+import DotsVertical from 'mdi-material-ui/DotsVertical'
+import CardHeader from '@mui/material/CardHeader'
+import Box from '@mui/material/Box'
+import { margin } from '@mui/system'
 
 // Styled component for the triangle shaped background image
 const TriangleImg = styled('img')({
@@ -15,10 +19,11 @@ const TriangleImg = styled('img')({
 
 // Styled component for the trophy image
 const TrophyImg = styled('img')({
-  right: 36,
   bottom: 20,
   height: 98,
-  position: 'absolute'
+  display: 'block',
+  marginLeft: 'auto'
+
 })
 
 const Trophy = () => {
@@ -27,20 +32,37 @@ const Trophy = () => {
   const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
 
   return (
-    <Card sx={{ position: 'relative' }}>
+    <Card sx={{ position: 'relative', borderRadius: 4.5 }}>
+      <CardHeader
+        title={`Congratulations Mr. Wilson's Class! 🥳`}
+        action={
+          <IconButton size='small' aria-label='settings' className='card-more-options' sx={{ color: 'text.secondary' }}>
+            <DotsVertical />
+          </IconButton>
+        }
+        subheader={
+          <Typography variant='body2'>
+            <Box component='span' sx={{ fontWeight: 600, color: 'text.primary' }}>
+            Your class watched the most videos
+            </Box>{' '}
+             this month!
+          </Typography>
+        }
+        titleTypographyProps={{
+          sx: {
+            mb: 2.5,
+            lineHeight: '2rem !important',
+            letterSpacing: '0.15px !important'
+          }
+        }}
+      />
       <CardContent>
-        <Typography variant='h6'>Congratulations Mr. Wilson's Class! 🥳</Typography>
-        <Typography variant='body2' sx={{ letterSpacing: '0.25px' }}>
-          Most Classes watched this month
-        </Typography>
-        <Typography variant='h5' sx={{ my: 4, color: 'primary.main' }}>
+        <Typography variant='h4' sx={{ my: 3, color: 'primary.main' }}>
           23 Videos Watched
+          <TrophyImg alt='trophy' src='/images/misc/trophy.png' />
         </Typography>
-        <Button size='small' variant='contained'>
-          View Awards
-        </Button>
         <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
-        <TrophyImg alt='trophy' src='/images/misc/trophy.png' />
+       
       </CardContent>
     </Card>
   )
